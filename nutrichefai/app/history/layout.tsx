3341ca416ebed2e0eb3a653e01162e8f4250e00a
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-11-27 17:44:00 -0800

    fix

--- nutrichefai/app/history/layout.tsx
+++ nutrichefai/app/history/layout.tsx
@@ -4,8 +4,8 @@ export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col md:flex-row h-[100dvh]">
-      <div className="flex-grow p-3 md:p-10 overflow-y-auto h-full">
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="flex-grow p-3 md:p-10 h-full min-h-0">
         <SessionProvider>{children}</SessionProvider>
       </div>
     </div>
